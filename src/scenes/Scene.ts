67c1fb8237cf336d78bde9e7dbc58cff1a88b089
Emma Grey <emgrey02@gmail.com>
2022-01-16 15:26:57 -0500

    Make scalable, remove back button logic

--- src/scenes/Scene.ts
+++ src/scenes/Scene.ts
@@ -1,30 +1,30 @@
-import { Container, TextStyle, Text } from "pixi.js";
-import { Manager } from "../Manager";
+import { Container, TextStyle, Text } from 'pixi.js';
+import { Manager } from '../Manager';
 
 export class Scene extends Container {
   private static titleStyle: TextStyle;
   private static paraStyle: TextStyle;
   private title: Text;
   private nextButton: HTMLButtonElement | null;
-  private backButton: HTMLButtonElement | null;
+  // private backButton: HTMLButtonElement | null;
 
   constructor(title: string) {
     super();
 
     Scene.titleStyle = new TextStyle({
-      fill: "#000000",
-      fontFamily: "Secular One",
+      fill: '#000000',
+      fontFamily: 'Secular One',
       fontSize: 52,
-      lineJoin: "round",
+      lineJoin: 'round',
       miterLimit: 0,
       padding: 15,
     });
 
     Scene.paraStyle = new TextStyle({
-      fill: "#000000",
-      fontFamily: "Mukta",
+      fill: '#000000',
+      fontFamily: 'Mukta',
       fontSize: 24,
-      lineJoin: "round",
+      lineJoin: 'round',
       miterLimit: 0,
       padding: 10,
     });
@@ -35,11 +35,11 @@ export class Scene extends Container {
     this.title.x = Manager.width / 2;
     this.title.y = Manager.height / 12;
 
-    this.nextButton = document.querySelector("#next");
-    this.backButton = document.querySelector("#back");
+    this.nextButton = document.querySelector('#next');
+    // this.backButton = document.querySelector("#back");
 
-    this.nextButton?.addEventListener("click", this.nextScreen);
-    this.backButton?.addEventListener("click", this.previousScreen);
+    this.nextButton?.addEventListener('click', this.nextScreen);
+    // this.backButton?.addEventListener("click", this.previousScreen);
   }
 
   static getTextStyle() {
@@ -52,53 +52,55 @@ export class Scene extends Container {
 
   nextScreen() {
     let nextKey: string;
-    let infoBlocks = document.querySelectorAll("p");
+    let infoBlocks = document.querySelectorAll('p');
     let infoBlocksArray = Array.from(infoBlocks);
     let nextInfoBlocks: HTMLElement[];
 
     let currentBlocks = infoBlocksArray.filter(
-      (el) => el.style.display !== "none"
+      (el) => el.style.display !== 'none'
     );
 
     const currentKey: string | undefined = currentBlocks[0].dataset.key;
     if (!currentKey) {
       return;
     }
+
     nextKey = `'${+currentKey + 1}'`;
     nextInfoBlocks = Array.from(
       document.querySelectorAll(`[data-key=${nextKey}]`)
     );
+
     if (nextInfoBlocks.length === 0) {
       return;
     }
 
-    infoBlocksArray.forEach((el) => (el.style.display = "none"));
-    nextInfoBlocks?.forEach((el) => (el.style.display = "block"));
+    infoBlocksArray.forEach((el) => (el.style.display = 'none'));
+    nextInfoBlocks?.forEach((el) => (el.style.display = 'block'));
   }
 
-  previousScreen() {
-    let nextKey: string;
-    let infoBlocks = document.querySelectorAll("p");
-    let infoBlocksArray = Array.from(infoBlocks);
-    let nextInfoBlocks: HTMLElement[];
+  // previousScreen() {
+  //   let nextKey: string;
+  //   let infoBlocks = document.querySelectorAll("p");
+  //   let infoBlocksArray = Array.from(infoBlocks);
+  //   let nextInfoBlocks: HTMLElement[];
 
-    let currentBlocks = infoBlocksArray.filter(
-      (el) => el.style.display !== "none"
-    );
+  //   let currentBlocks = infoBlocksArray.filter(
+  //     (el) => el.style.display !== "none"
+  //   );
 
-    const currentKey: string | undefined = currentBlocks[0].dataset.key;
-    if (!currentKey) {
-      return;
-    }
-    nextKey = `'${+currentKey - 1}'`;
-    nextInfoBlocks = Array.from(
-      document.querySelectorAll(`[data-key=${nextKey}]`)
-    );
-    if (nextInfoBlocks.length === 0) {
-      return;
-    }
+  //   const currentKey: string | undefined = currentBlocks[0].dataset.key;
+  //   if (!currentKey) {
+  //     return;
+  //   }
+  //   nextKey = `'${+currentKey - 1}'`;
+  //   nextInfoBlocks = Array.from(
+  //     document.querySelectorAll(`[data-key=${nextKey}]`)
+  //   );
+  //   if (nextInfoBlocks.length === 0) {
+  //     return;
+  //   }
 
-    infoBlocksArray.forEach((el) => (el.style.display = "none"));
-    nextInfoBlocks?.forEach((el) => (el.style.display = "block"));
-  }
+  //   infoBlocksArray.forEach((el) => (el.style.display = "none"));
+  //   nextInfoBlocks?.forEach((el) => (el.style.display = "block"));
+  // }
 }
